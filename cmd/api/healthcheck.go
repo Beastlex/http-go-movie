001b20437b739a31314342d0aede22e90cd479d3
beastlex <beastlex@gmail.com>
2022-04-08 03:49:15 +0000

    2022-04-08: healthcheck to json format

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,7 +6,11 @@ import (
 )
 
 func (app *application) healthcheckerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "env: %s\n", app.config.env)
-	fmt.Fprintf(w, "ver: %s\n", version)
+	js := `{"status": "available", "env": %q, "ver": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// Set correct content type in header
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(js))
 }
