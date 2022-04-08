0ccef51f564edad4423f969721fb9188da4284c7
beastlex <beastlex@gmail.com>
2022-04-08 04:52:42 +0000

    2022-04-08: change fileds to display

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,10 +4,10 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"` // Use the - directive
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`
-	Runtime   int32     `json:"runtime"` //in minutes
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"` //in minutes
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"` // inc each time the movie info is updates
 }
