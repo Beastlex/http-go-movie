465168bb13d3004efa86ad0bb20156f66634903b
beastlex <beastlex@gmail.com>
2022-04-08 04:49:46 +0000

    2022-04-08: fix json fields name

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,11 +3,11 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     `json: "id"`
-	CreatedAt time.Time `json: "created_at"`
-	Title     string    `json: "title"`
-	Year      int32     `json: "year"`
-	Runtime   int32     `json: "runtime"` //in minutes
-	Genres    []string  `json: "genres"`
-	Version   int32     `json: "version"` // inc each time the movie info is updates
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year"`
+	Runtime   int32     `json:"runtime"` //in minutes
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"` // inc each time the movie info is updates
 }
