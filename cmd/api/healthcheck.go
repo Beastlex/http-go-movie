1a720032dd829a86ff22e1298e61be66d79ee067
beastlex <beastlex@gmail.com>
2022-04-08 04:04:33 +0000

    2022-04-08: use json marshaling

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckerHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "env": %q, "ver": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	data := map[string]string{
+		"status": "available",
+		"env":    app.config.env,
+		"ver":    version,
+	}
 
-	// Set correct content type in header
-	w.Header().Set("Content-Type", "application/json")
-
-	w.Write([]byte(js))
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Server could not process your request", http.StatusInternalServerError)
+	}
 }
