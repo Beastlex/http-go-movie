97326c16c1edc24504e0ff5e07dcff97b6626ae1
beastlex <beastlex@gmail.com>
2022-04-09 05:18:42 +0000

    2022-04-09: add db postgres support

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "0.1.0"
@@ -14,6 +18,9 @@ const version = "0.1.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -27,10 +34,19 @@ func main() {
 	// Read command-line flags
 	flag.IntVar(&cfg.port, "port", 4000, "API listen port")
 	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stag|prod)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight", "PostgreSQL DSN")
 	flag.Parse()
 
 	// Create stdout logger
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Connect to DB
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+	logger.Println("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -47,6 +63,24 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	//Create context with 5-second deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
